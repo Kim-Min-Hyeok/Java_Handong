91d524d0605fdd93ff34707c0daefb44497d041a
Kim-Min-Hyeok <21900102@handong.edu>
2020-05-14 23:30:37 +0900

    Java프로그래밍 1번 프로젝트

---
+++ CH01_Start/src/KimMinHyeok.java
@@ -0,0 +1,9 @@
+//ÇÐ¹ø : 21900102 ÀÌ¸§ : ±è¹ÎÇõ
+import java.util.Scanner;
+public class KimMinHyeok {
+	public static void main (String [] args) {
+		System.out.println("Name : KimMinHyeok");
+		System.out.println("University : Handong Global University");
+		System.out.println("Student Number 12345678");
+	}
+}
